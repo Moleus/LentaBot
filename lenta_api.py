e3b7506a15973f40e2a790497a0ac660031def8d
Moleus <fafufuburr@gmail.com>
2020-07-09 23:15:36 +0300

    Fix discount date format

--- lenta_api.py
+++ lenta_api.py
@@ -15,8 +15,8 @@ def get_json_from_url(url, headers):
 
 def get_response(url, headers):
     """
-    Requests a page by given url.
-    Returns: html page in text format.
+    Requests a reponse by given url.
+    Returns: http response.
     """
     session = requests.Session()
     response = session.get(url, headers=headers)
@@ -30,18 +30,31 @@ def fetch_good_info(page_text):
     Returns Good name, price(with promocard),
     promo state, promo period(с 23.03 по 01.04)
     """
-    print(page_text)
     soup = BeautifulSoup(page_text, 'html.parser')
     good_info_container = soup.find("div", class_="sku-page-control-container sku-page__control")
     good_info = good_info_container.attrs.get("data-model")
 
-    price = int(json.loads(good_info)['cardPrice']['integerPart'])
+    price = round(json.loads(good_info)['cardPrice']['value'])
     title = json.loads(good_info)['title']
     isPromoForCardPrice = json.loads(good_info)['isPromoForCardPrice']
     if isPromoForCardPrice:
-        promoStart = "".join(json.loads(good_info)['promoStart'][5:10].replace("-", "."))
-        promoEnd = "".join(json.loads(good_info)['promoEnd'][5:10].replace("-", "."))
-        promoDate = "с %s по %s" % (promoStart, promoEnd)
+        print(json.loads(good_info)['promoStart'])
+        print(json.loads(good_info)['promoEnd'])
+        # promoStart
+        promo_start_string = json.loads(good_info)['promoStart']
+        promo_start_list = promo_start_string[5:10].split("-")
+        promo_start_list.reverse()
+        promo_start = ".".join(promo_start_list)
+        # promoEnd
+        promo_end_string = json.loads(good_info)['promoEnd']
+        promo_end_list = promo_end_string[5:10].split("-")
+        promo_end_list.reverse()
+        promo_end = ".".join(promo_end_list)
+        # promoEnd = "".join((json.loads(good_info)['promoEnd'][5:10].replace("-", ".")).reverse())
+        print(promo_start)
+        print(promo_end)
+
+        promoDate = "с %s по %s" % (promo_start, promo_end)
     else:
         promoDate = ""
 
